65dd13a17a99f0141d4278fb09a48f20d3b8f66a
Anton4441 <168655083+Anton4441@users.noreply.github.com>
2024-07-31 00:44:13 +0700

    Add files via upload

--- aaaa/js/main.js
+++ aaaa/js/main.js
@@ -1,16 +1,29 @@
-(function() {
+(function () {
     document.addEventListener('click', burgerInit)
     function burgerInit(e) {
         const burgerIcon = e.target.closest('.burger-icon')
         const navLink = e.target.closest('.nav__link')
         if (!burgerIcon && !navLink) return
-
         if (document.documentElement.clientWidth > 900) return
-
         if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu')
         } else {
             document.body.classList.remove('body--opened-menu')
+        }
+    }
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+    modal.addEventListener('click', closeModal)
+    modalButton.addEventListener('click', openModal)
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+    function closeModal(e) {
+        e.preventDefault()
+        const target = e.target
+        if(target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
         }
     }
 })()
